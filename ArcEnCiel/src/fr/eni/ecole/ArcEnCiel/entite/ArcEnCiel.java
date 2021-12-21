04e29aab98fe268c1b2d61a5a9b2e38e03b1eb66
nora-sch <nora.schumann.development@gmail.com>
2021-12-21 12:57:33 +0100

    ENUMERATION ArcEnCiel etape 1 et 2

---
+++ ArcEnCiel/src/fr/eni/ecole/ArcEnCiel/entite/ArcEnCiel.java
@@ -0,0 +1,26 @@
+package fr.eni.ecole.ArcEnCiel.entite;
+
+public enum ArcEnCiel {
+	ROUGE, ORANGE, JAUNE, VERT, BLEU, INDIGO, VIOLET;
+
+	public static ArcEnCiel selectionneInstance(String value){
+
+		try {
+			return ArcEnCiel.valueOf(value.toUpperCase());
+		} catch (IllegalArgumentException | NullPointerException e) {
+			// Permet de gérer la saisie d'une valeur qui n'existe pas
+			return ORANGE;
+		}
+	}
+	
+	public static ArcEnCiel suivante(ArcEnCiel couleur){
+		int pos = couleur.ordinal();  
+
+		int nextPos = pos+1;
+		if (nextPos >= ArcEnCiel.values().length){
+			nextPos = 0;
+		}
+		return ArcEnCiel.values()[nextPos];
+	}
+
+}
---
+++ ArcEnCiel/src/fr/eni/ecole/ArcEnCiel/test/TestArcEnCiel.java
@@ -0,0 +1,53 @@
+package fr.eni.ecole.ArcEnCiel.test;
+import fr.eni.ecole.ArcEnCiel.entite.ArcEnCiel;
+
+public class TestArcEnCiel {
+	public static void main(String[] args) {
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Etape 1 -- Test de l'instance d'ArcEnCiel");
+		System.out.println("-----------------------------------------------------");
+		System.out.println("Sélection de la couleur jaune");
+		ArcEnCiel couleurJaune = ArcEnCiel.selectionneInstance("jaune");
+		System.out.println("L'instance de l'énumération est " + couleurJaune);
+		
+		System.out.println();
+		System.out.println("Sélection de la couleur VIOLET");
+		ArcEnCiel couleurVIOLET = ArcEnCiel.selectionneInstance("VIOLET");
+		System.out.println("L'instance de l'énumération est " + couleurVIOLET);
+		
+		System.out.println();
+		System.out.println("Sélection de la couleur blanche implique la récupération de la couleur orange");
+		ArcEnCiel couleurBlanche = ArcEnCiel.selectionneInstance("blanche");
+		System.out.println("L'instance de l'énumération est " + couleurBlanche);
+		
+		System.out.println();
+		System.out.println("Sélection d'une valeur nule implique la récupération de la couleur orange");
+		ArcEnCiel couleurNull = ArcEnCiel.selectionneInstance(null);
+		System.out.println("L'instance de l'énumération est " + couleurNull);
+	
+		
+		System.out.println("\n-----------------------------------------------------");
+		System.out.println("Etape 2 -- Récupération de la valeur suivante");
+		System.out.println("-----------------------------------------------------");
+		System.out.println("La couleur qui suit jaune  ->  vert");
+		ArcEnCiel suivanteJaune = ArcEnCiel.suivante(couleurJaune);
+		System.out.println("Vérification de l'instance qui suit jaune -- " + suivanteJaune);
+		
+		System.out.println();
+		System.out.println("La couleur qui suit violet  ->  rouge");
+		ArcEnCiel suivanteViolet = ArcEnCiel.suivante(couleurVIOLET);
+		System.out.println("Vérification de l'instance qui suit violet -- " + suivanteViolet);
+		
+		System.out.println();
+		System.out.println("La couleur qui suit orange  ->  jaune");
+		ArcEnCiel suivanteOrange = ArcEnCiel.suivante(couleurBlanche);
+		System.out.println("Vérification de l'instance qui suit orange -- " + suivanteOrange);
+		
+		
+//		System.out.println("\n-----------------------------------------------------");
+//		System.out.println("Etape 3 -- Affichage de toutes les valeurs");
+//		System.out.println("-----------------------------------------------------");
+//		ArcEnCiel.afficheToutArcEnCiel();
+//	
+	}
+}
