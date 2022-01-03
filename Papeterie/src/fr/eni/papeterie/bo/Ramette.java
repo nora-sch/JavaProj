26e11d858d8d07b61b85bb9e9d5eb2431c46f5d0
nora-sch <nora.schumann.development@gmail.com>
2022-01-03 15:25:24 +0100

    add articles

--- Papeterie/src/fr/eni/papeterie/bo/Ramette.java
+++ Papeterie/src/fr/eni/papeterie/bo/Ramette.java
@@ -1,5 +1,37 @@
 package fr.eni.papeterie.bo;
 
-public class Ramette {
+public class Ramette extends Article{
+	int grammage;
 
+//	public Ramette() {
+//		
+//	};
+
+	public Ramette(Integer idArticle, String marque, String ref, String designation, float pu, int qte, int grammage) {
+		super(idArticle, marque, ref, designation, pu, qte);
+		this.grammage = grammage;
+	}
+	public Ramette(String marque, String ref, String designation, float pu, int qte, int grammage) {
+		super(marque, ref, designation, pu, qte);
+		this.grammage = grammage;
+	}
+ // getters et setters
+	/**
+	 * @return the grammage
+	 */
+	public int getGrammage() {
+		return grammage;
+	}
+	/**
+	 * @param grammage the grammage to set
+	 */
+	public void setGrammage(int grammage) {
+		this.grammage = grammage;
+	}
+	
+	// méthodes
+	@Override
+	public String toString() {
+		return "grammage - "+ grammage;
+	}
 }
--- Papeterie/src/fr/eni/papeterie/bo/Stylo.java
+++ Papeterie/src/fr/eni/papeterie/bo/Stylo.java
@@ -1,5 +1,39 @@
 package fr.eni.papeterie.bo;
 
-public class Stylo {
+public class Stylo extends Article{
+	String couleur;
+	
+//	public Stylo() {
+//	
+//	}
+	public Stylo(Integer idArticle, String marque, String ref, String designation, float pu, int qte, String couleur) {
+		super(idArticle, marque, ref, designation, pu, qte);
+		this.couleur = couleur;
+	}
+	public Stylo(String marque, String ref, String designation, float pu, int qte, String couleur) {
+		super(marque, ref, designation, pu, qte);
+		this.couleur = couleur;
+	}
+	
+	// getters et setters
+	/**
+	 * @return the couleur
+	 */
+	public String getCouleur() {
+		return couleur;
+	}
+	/**
+	 * @param couleur the couleur to set
+	 */
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+	
+	// méthodes
+		@Override
+		public String toString() {
+			return "couleur - "+ couleur;
+		}
+	
 
 }
