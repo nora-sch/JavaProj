1a85834e253ea92b6d14b42c1fe07d0c62248d50
nora-sch <nora.schumann.development@gmail.com>
2022-01-05 14:58:45 +0100

    add DAL layer

---
+++ Papeterie/src/fr/eni/papeterie/dal/jdbc/ArticleDaoJdbcImpl.java
@@ -0,0 +1,32 @@
+package fr.eni.papeterie.dal.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.papeterie.bo.Article;
+
+public class ArticleDaoJdbcImpl {
+
+	
+	public void insert(Article a) {
+
+	}
+	public void update(Article a) {
+
+	}
+	public Article selectById(int idArticle) {
+		return null;
+	}
+	
+	public List<Article> selectAll() {
+				return null;
+
+	}
+	public void delete(Article a) {
+
+	}
+}
