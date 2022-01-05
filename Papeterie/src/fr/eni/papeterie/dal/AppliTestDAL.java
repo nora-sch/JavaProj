0b1391e3e2c5caee2fd8d4a675f8a5b75df4618f
nora-sch <nora.schumann.development@gmail.com>
2022-01-05 18:47:48 +0100

    error in preparedStatement set of insert - l'index 1 est hors limites

--- Papeterie/src/fr/eni/papeterie/dal/AppliTestDAL.java
+++ Papeterie/src/fr/eni/papeterie/dal/AppliTestDAL.java
@@ -6,14 +6,14 @@ import fr.eni.papeterie.bo.Article;
 import fr.eni.papeterie.bo.Ramette;
 import fr.eni.papeterie.bo.Stylo;
 import fr.eni.papeterie.dal.DALException;
-import fr.eni.papeterie.dal.jdbc.ArticleDAOJdbcImpl;
+import fr.eni.papeterie.dal.jdbc.ArticleDaoJdbcImpl;
 
 public class AppliTestDAL {
 
 	public static void main(String[] args) {
 
 		//Déclaration et instanciation de la DAO
-		ArticleDAOJdbcImpl articleDAO = new ArticleDAOJdbcImpl();
+		ArticleDaoJdbcImpl articleDAO = new ArticleDaoJdbcImpl();
 
 		//Instanciation du jeu d'essai 
 		Article a1 = new Stylo( "Bic", "BBOrange","Bic bille Orange", 1.2f, 20, "bleu");
@@ -31,40 +31,40 @@ public class AppliTestDAL {
 			articleDAO.insert(a3);
 			System.out.println("Article ajouté  : " + a3.toString() );
 
-
-			//Sélection de l'article par id
-			Article a = articleDAO.selectById(a2.getIdArticle());
-			System.out.println("\nSélection de l'article par id  : " + a.toString() );
-
-			//Sélection de tous les articles
-			List<Article> articles = articleDAO.selectAll();
-			System.out.println("\nSélection de tous les articles  : "  );
-			afficherArticles(articles);
-
-			//Modification d'un article
-			System.out.println("\nModification d'un article  : " );
-			System.out.println("Article avant modification : "  + a1.toString());
-			((Stylo) a1).setCouleur("noir");
-			((Stylo) a1).setDesignation("Bic bille noir");
-			((Stylo) a1).setReference("BBNoir");
-			articleDAO.update(a1);
-			System.out.println("Article après modification  : " + a1.toString() );
-			
-			
-			//Suppression d'un article
-			System.out.println("\nSuppression de l'article  : " + a1.toString());
-			articleDAO.delete(a1.getIdArticle());
-			articles = articleDAO.selectAll();
-			System.out.println("Liste des articles après suppression : "  );
-			afficherArticles(articles);
-			System.out.println("---------------------------------------------------------------");
-
-			
-		} catch (DALException e) {
+//
+//			//Sélection de l'article par id
+//			Article a = articleDAO.selectById(a2.getIdArticle());
+//			System.out.println("\nSélection de l'article par id  : " + a.toString() );
+//
+//			//Sélection de tous les articles
+//			List<Article> articles = articleDAO.selectAll();
+//			System.out.println("\nSélection de tous les articles  : "  );
+//			afficherArticles(articles);
+//
+//			//Modification d'un article
+//			System.out.println("\nModification d'un article  : " );
+//			System.out.println("Article avant modification : "  + a1.toString());
+//			((Stylo) a1).setCouleur("noir");
+//			((Stylo) a1).setDesignation("Bic bille noir");
+//			((Stylo) a1).setReference("BBNoir");
+//			articleDAO.update(a1);
+//			System.out.println("Article après modification  : " + a1.toString() );
+//			
+//			
+//			//Suppression d'un article
+//			System.out.println("\nSuppression de l'article  : " + a1.toString());
+//			articleDAO.delete(a1.getIdArticle());
+//			articles = articleDAO.selectAll();
+//			System.out.println("Liste des articles après suppression : "  );
+//			afficherArticles(articles);
+//			System.out.println("---------------------------------------------------------------");
+//
+//			
+	} catch (DALException e) {
 			e.printStackTrace();
 		}
 
-	}
+}
 
 	
 	private static void afficherArticles(List<Article> articles){
--- Papeterie/src/fr/eni/papeterie/dal/jdbc/ArticleDaoJdbcImpl.java
+++ Papeterie/src/fr/eni/papeterie/dal/jdbc/ArticleDaoJdbcImpl.java
@@ -1,29 +1,117 @@
 package fr.eni.papeterie.dal.jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
+import java.sql.Types;
 import java.util.List;
 
+
 import fr.eni.papeterie.bo.Article;
+import fr.eni.papeterie.bo.Ramette;
+import fr.eni.papeterie.bo.Stylo;
+import fr.eni.papeterie.dal.DALException;
 
 public class ArticleDaoJdbcImpl {
+	private static final String TYPE_STYLO = "STYLO";
+	private static final String TYPE_RAMETTE = "RAMETTE";
 
-	
-	public void insert(Article a) {
+	private Connection connection = null;
+
+	static {
+		// charger le driver jdbc en mémoire
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ArticleDaoJdbcImpl() {
 
 	}
+	public Connection getConnection() throws SQLException {
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=PAPETERIE_DB";
+		connection = DriverManager.getConnection(url, "sa", "rt");
+		return connection;
+	}
+
+	public void closeConnection(){
+		if(connection != null){
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			connection=null;
+		}
+	}
+
+	public void insert(Article a) throws DALException  {
+		String sql = "INSERT INTO Articles (reference,marque,designation,prixUnitaire,qteStock,type,grammage,couleur) VALUES ('?, ?, ?, ?, ?, ?, ?, ?)";
+		Connection con = null;
+		PreparedStatement stmt = null;
+		try {
+			con = getConnection();
+			stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			// --- error L'index 1 est hors limites!
+			System.out.println(a.getReference());
+			stmt.setString(1, a.getReference());
+			stmt.setString(2, a.getMarque());
+			stmt.setString(3, a.getDesignation());
+			stmt.setFloat(4, a.getPrixUnitaire());
+			stmt.setInt(5, a.getQteStock());
+			if (a instanceof Ramette) {
+				Ramette r = (Ramette) a;
+				stmt.setString(6, TYPE_RAMETTE);
+				stmt.setInt(7, r.getGrammage());
+				stmt.setNull(8, Types.VARCHAR);
+			}
+			if (a instanceof Stylo) {
+				Stylo s = (Stylo) a;
+				stmt.setString(6, TYPE_STYLO);
+				stmt.setNull(7, Types.INTEGER);
+				stmt.setString(8, s.getCouleur());
+			}
+			// ---------------------------
+			int result = stmt.executeUpdate();
+			// récuperer et set dernière id géneré à l'instance d'Article
+			if (result == 1) {
+				ResultSet rs = stmt.getGeneratedKeys();
+				System.out.println(rs);
+				if (rs.next()) {
+					a.setIdArticle(rs.getInt(1));
+				}
+			}
+
+		} catch (SQLException e) {
+			throw new DALException("Insert article failed - " + a, e);
+		}
+		finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+
+			} catch (SQLException e) {
+				throw new DALException("close failed - ", e);
+			}
+			closeConnection();
+		}
+	}
+
 	public void update(Article a) {
 
 	}
 	public Article selectById(int idArticle) {
 		return null;
 	}
-	
+
 	public List<Article> selectAll() {
-				return null;
+		return null;
 
 	}
 	public void delete(Article a) {
